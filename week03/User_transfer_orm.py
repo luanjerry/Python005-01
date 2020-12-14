84eb1eb8593d32d3d90e1488802fa4c0fdcd62ab
luanjerry <2466581516@qq.com>
2020-12-14 20:45:00 +0800

    debug_1
    修改了几个错误，规范几个写法，审计的部分出现了错误，需要进一步修改

--- week03/User_transfer_orm.py
+++ week03/User_transfer_orm.py
@@ -14,6 +14,29 @@ from datetime import datetime
 请实现转账 100 极客币的 SQL(可以使用 pymysql 或 sqlalchemy-orm 实现)，
 张三余额不足，转账过程中数据库 crash 等情况需保证数据一致性
 用户表
+
+### this is a **better** (but not the only) way to do it ###
+
+class ThingOne(object):
+    def go(self, session):
+        session.query(FooBar).update({"x": 5})
+
+class ThingTwo(object):
+    def go(self, session):
+        session.query(Widget).update({"q": 18})
+
+def run_my_program():
+    session = Session()
+    try:
+        ThingOne().go(session)
+        ThingTwo().go(session)
+
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 """
 
 
@@ -39,29 +62,96 @@ class Audit_table(Base):
     id = Column(Integer(), primary_key=True)
     transfer_time = Column(DateTime(), default=datetime.now)
     # 被转用户
-    username_in = Column(String(20), nullable=False, unique=True)
+    username_in = Column(String(20), nullable=False, unique=False)
     # 被转用户id
     inid = Column(Integer(), ForeignKey(Users_table.uid))
     # 转账用户
-    username_out = Column(String(20), nullable=False, unique=True)
+    username_out = Column(String(20), nullable=False, unique=False)
     # 转账用户id
     outid = Column(Integer(), ForeignKey(Users_table.uid))
     transfer_amount = Column(Integer())
 
+
+
+
+class Thing:
+
+    def __init__(self):
+        pass
+    @classmethod
+    def go(cls,session,a):
+        
+        #获取转账用户名
+        namea= session.query(Users_table.username).filter(Users_table.uid == a).first()
+        return namea[0]
+
+class Thing2:
+    @classmethod
+    def go(cls,session,b):
+        #获取收款用户名
+        nameb= session.query(Users_table.username).filter(Users_table.uid == b).first()
+        return nameb[0]
+       
+
+class Thing3:
+    @classmethod
+    def go(cls,session,a,qian):
+        #扣款
+        
+        kou = session.query(Assets_table.username, Assets_table.zid, Assets_table.assetsnum).filter(Assets_table.zid == a)
+        print(kou.first()[2],"==",type(kou.first()[2]))
+        print(103)
+        having = kou.first()[2]
+        if having <qian:
+            print(106)
+            raise
+        else:
+            kou.update({Assets_table.assetsnum: having-qian})
+            print("扣钱了")
+
+
+class Thing4:
+    @classmethod
+    def go(cls,session,b,qian):
+        # 收款
+        jia = session.query(Assets_table.username, Assets_table.zid, Assets_table.assetsnum).filter(Assets_table.zid == b)
+        print(122)
+        having = jia.first()[2]
+        jia.update({Assets_table.assetsnum: having + qian})
+        print("发钱了")
+
+class Thing5:
+    @classmethod
+    def go(cls,session,a,b,aa,bb,m):
+        #记录到转账审计表
+        h = Audit_table(username_out= aa, username_in= bb, outid= a, inid= b, transfer_amount= m)
+        session.add(h)
+        #res = session.query(Audit_table.outid, Audit_table.username_out, Audit_table.inid,\
+                   # Audit_table.username_in, Audit_table.transfer_amount, Audit_table.transfer_amount).all()
+        #print(129)
+        #session.query(Audit_table.outid, Audit_table.username_out, Audit_table.inid, Audit_table.username_in, Audit_table.transfer_amount)
+        print(" -----132------转账审计审计表 数据表(audit)")
+#add8 = Tuser_table(username='kiki', age=28, birthday='1992-11-3', sex='女', education='本科')
+#session.add(add8)
+
 # 用户操作类
 class User_actions():
-    def __init__(self):
-        self.dburl = "mysql+pymysql://testuser:testpass#AB1234@192.168.3.38:3306/testdb?charset=utf8mb4"
-        self.engine = create_engine(self.dburl, echo=True, encoding="utf-8")
 
-    # 转账操作
+    def __init__(self):
+        pass
+        #self.dburl = "mysql+pymysql://testuser:testpass#AB1234@192.168.3.38:3306/testdb?charset=utf8mb4"
+        #self.engine = create_engine(self.dburl, echo=True, encoding="utf-8")
+
+    '''# 转账操作
     def Transfer_deposite(self,oname, iname, nums):
         SessionClass = sessionmaker(bind=self.engine)
         session = SessionClass()
         user_out = oname
         user_in = iname
+
+        
         # 查询转账用户id
-        zz_user = session.query(Users_table.username, Users_table.uid).filter(Users_table.username == user_out).first()
+        zz_user =  session.query(Users_table.username).filter(Users_table.uid == a).first()
         # 获取转账用户id
         zz_num = int(zz_user[1])
 
@@ -72,28 +162,34 @@ class User_actions():
 
         # 转账用户出账操作
         zz_user_ssets = session.query(Assets_table.username, Assets_table.zid, \
-                                     Assets_table.assetsnum).filter(Assets_table.zid == zz_num)
+                                    Assets_table.assetsnum).filter(Assets_table.zid == zz_num)
         zz_user_ssets.update({Assets_table.assetsnum: int(zz_user_ssets.first()[2])-nums})
+        #need check 
         print(zz_user_ssets.first())
 
         # 被账用户进账操作
         bz_user_ssets = session.query(Assets_table.username, Assets_table.zid, \
-                                      Assets_table.assetsnum).filter(Assets_table.zid == bz_num)
+                                    Assets_table.assetsnum).filter(Assets_table.zid == bz_num)
         bz_user_ssets.update({Assets_table.assetsnum: int(bz_user_ssets.first()[2]) + nums})
         print(bz_user_ssets.first())
 
         #记录到转账审计表
         auditadd=Audit_table(username_out=oname, username_in=iname, outid=zz_num, inid=bz_num, transfer_amount=nums)
         session.add(auditadd)
+
         res = session.query(Audit_table.outid, Audit_table.username_out, Audit_table.inid,\
-                      Audit_table.username_in, Audit_table.transfer_amount, Audit_table.transfer_amount).all()
+                    Audit_table.username_in, Audit_table.transfer_amount, Audit_table.transfer_amount).all()
         print(" -----------转账审计审计表 数据表(audit)")
+
         for i in res:
             print(i)
         print(" -----------转转用户资产表 数据表(userassets)：", zz_user_ssets.first())
         print(" -----------被转用户资产表 数据表(userassets): ", bz_user_ssets.first())
         #session.flush()
-        session.commit()
+        session.commit() '''
+
+     
+
     # 添加用户
     def user_add(self, usadd):
         SessionClass = sessionmaker(bind=self.engine)
@@ -108,12 +204,12 @@ class User_actions():
         session.add(asadd)
         session.commit()
 
-dburl="mysql+pymysql://testuser:testpass#AB1234@192.168.3.38:3306/testdb?charset=utf8mb4"
-engine = create_engine(dburl, echo=True, encoding="utf-8")
-#Base.metadata.create_all(engine)
+#dburl="mysql+pymysql://testuser:testpass#AB1234@192.168.3.38:3306/testdb?charset=utf8mb4"
+#engine = create_engine(dburl, echo=True, encoding="utf-8")
+'''Base.metadata.create_all(engine)
 
 #增加用户
-"""
+
 add_h1 = Users_table(uid=100, username='tom1')
 add_h2 = Users_table(uid=101, username='tom2')
 add_h3 = Users_table(uid=102, username='jack1')
@@ -123,9 +219,9 @@ uadd.user_add(add_h1)
 uadd.user_add(add_h2)
 uadd.user_add(add_h3)
 uadd.user_add(add_h4)
-"""
+
 #增加用户资产
-""""
+
 add_z1 = Assets_table(zid=100, username='tom1', assetsnum=2000 )
 add_z2 = Assets_table(zid=101, username='tom2', assetsnum=2000)
 add_z3 = Assets_table(zid=102, username='jack1', assetsnum=2000)
@@ -135,7 +231,49 @@ zadd.assets_add(add_z1)
 zadd.assets_add(add_z2)
 zadd.assets_add(add_z3)
 zadd.assets_add(add_z4)
-"""
+'''
 #用户转账
-zzuser = User_actions()
-zzuser.Transfer_deposite('tom1', 'jack1', 100)
+# show_me_the_money = User_actions()
+
+#def a_to_b():
+
+#show_me_the_money.Transfer_deposite('tom1', 'jack1', 100)
+
+
+def run_my_program(db,a,b,m):
+
+    dburl= db
+    engine = create_engine(dburl, echo=True, encoding="utf-8")
+    SessionClass = sessionmaker(bind=engine)
+    session = SessionClass()
+
+    #session = Session()
+    try:
+        print(242)
+        
+        aa= Thing().go(session,a)
+        print(246)
+        bb= Thing2().go(session,b)
+        #a 扣款
+        Thing3().go(session,a,m)
+        #b 收款
+        Thing4().go(session,b,m)
+        # 记录审计
+        #Thing5().go(session,a,b,aa,bb,m)     --还有问题，需要改
+        print(260)
+        session.commit()
+        print(262)
+    except:
+        session.rollback()
+        #raise
+        print("I am sorry!")
+    finally:
+        session.close()
+
+if __name__ == '__main__':
+    db="mysql+pymysql://testuser:testpass#AB1234@192.168.3.38:3306/testdb?charset=utf8mb4"
+    a = 100
+    b = 102
+    money =200
+    if money > 0:
+        run_my_program(db,a,b,money)
--- week03/week3.py
+++ week03/week3.py
@@ -6,36 +6,104 @@ from sqlalchemy import func
 from sqlalchemy import desc
 from datetime import datetime
 
-
-"""
-import pymysql
+#import pymysql
  
 # 打开数据库连接
-db = pymysql.connect("192.168.3.38","testuser","testpass#AB1234","testdb" )
+#db = pymysql.connect("192.168.3.38","testuser","testpass#AB1234","testdb" )
  
 # 使用 cursor() 方法创建一个游标对象 cursor
-cursor = db.cursor()
+#cursor = db.cursor()
  
 # 使用 execute() 方法执行 SQL，如果表存在则删除
-cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
- 
+#cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+''' 
 # 使用预处理语句创建表
 sql = """CREATE TABLE EMPLOYEE (
          FIRST_NAME  CHAR(20) NOT NULL,
          LAST_NAME  CHAR(20),
          AGE INT,  
          SEX CHAR(1),
-         INCOME FLOAT )"""
+         INCOME FLOAT )""" '''
+'''
+sql = "select * from blogs"
+try:
+    cur.execute(sql)  # 执行sql语句
+
+    results = cur.fetchall()  # 获取查询的所有记录
+    print("id", "user_id", "name")
+    # 遍历结果
+    for row in results:
+        id = row[0]
+        user_id = row[1]
+        name = row[4]
+        print(id, name, user_id)
+except Exception as e:
+    raise e
+finally:
+    db.close()  # 关闭连接
+# 2.插入操作
+db = pymysql.connect("localhost","root","mysql12345","awesome")
+cur_insert = db.cursor()
+
+# sql插入语句 表名blogs
+sql_insert ="""insert into blogs(id,user_id,name) values ("test_id",'test_user_id','test_name')"""
+
+try:
+    cur_insert.execute(sql_insert)
+    # 提交
+    db.commit()
+    print('开始数据库插入操作')
+except Exception as e:
+    db.rollback()
+    print('数据库插入操作错误回滚')
+finally:
+    db.close()
+# 打开数据库连接
+db = pymysql.connect("localhost","testuser","test123","TESTDB" )
  
-cursor.execute(sql)
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
+ 
+# SQL 插入语句
+sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+try:
+   # 执行sql语句
+   cursor.execute(sql)
+   # 提交到数据库执行
+   db.commit()
+except:
+   # 如果发生错误则回滚
+   db.rollback()
  
 # 关闭数据库连接
 db.close()
 
-"""
+# SQL 插入语句
+sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000),('Mac1', 'Mohan', 20, 'M', 2000),('Mac2', 'Mohan2', 20, 'M', 2000),('Mac33', 'Mohan3', 20, 'M', 2000)"""
+try:
+   # 执行sql语句
+   cursor.execute(sql)
+   # 提交到数据库执行
+   db.commit()
+   print("CREATE TABLE OK")
+except:
+   # 如果发生错误则回滚
+   db.rollback()
+ 
+# 关闭数据库连接
+db.close() 
+ # test for insert ,pass 
+#cursor.execute(sql)
 
+#print("CREATE TABLE OK")
+# 关闭数据库连接
+#db.close()
 
-
+'''
 
 """
 使用 sqlalchemy ORM 方式创建如下表，使用 PyMySQL 对该表写入 3 条测试数据，并读取:
@@ -59,12 +127,16 @@ class Tuser_table(Base):
 #dburl="mysql+pymysql://testuser:testpass@127.0.0.1:3306/testdb?charset=utf8mb4"
 dburl="mysql+pymysql://testuser:testpass#AB1234@192.168.3.38:3306/testdb?charset=utf8mb4"
 engine = create_engine(dburl, echo=True, encoding="utf-8")
+
+# the first time open this line to create the table
 #Base.metadata.create_all(engine)
 
 SessionClass = sessionmaker(bind=engine)
 session = SessionClass()
 
-#添加数据
+'''
+# 
+#添加数据 , 添加一次数据后，注释
 add1 = Tuser_table(username='tom1', age=33, birthday='1987-1-1', sex='男', education='本科')
 add2 = Tuser_table(username='tom2', age=39, birthday='1981-2-7', sex='男', education='专科')
 add3 = Tuser_table(username='jack1', age=30, birthday='1990-3-4', sex='男', education='专科')
@@ -73,15 +145,17 @@ add5 = Tuser_table(username='yuyu', age=30, birthday='1992-11-3', sex='男', edu
 add6 = Tuser_table(username='peter', age=29, birthday='1991-4-3', sex='女', education='本科')
 add7 = Tuser_table(username='luxi', age=28, birthday='1992-11-3', sex='女', education='本科')
 add8 = Tuser_table(username='kiki', age=28, birthday='1992-11-3', sex='女', education='本科')
-#session.add(add1)
-#session.add(add2)
-#session.add(add3)
-#session.add(add4)
-#session.add(add5)
-#session.add(add6)
-#session.add(add7)
-#session.add(add8)
-#session.commit()
+session.add(add1)
+session.add(add2)
+session.add(add3)
+session.add(add4)
+session.add(add5)
+session.add(add6)
+session.add(add7)
+session.add(add8)
+session.commit()
+
+'''
 
 
 #查询数据
@@ -96,9 +170,12 @@ for i in result:
     print(i)
 
 #条件查询根据学历和性别
+#yy = session.query(Tuser_table.username, Tuser_table.age,  func.date_format(Tuser_table.birthday, "%Y-%m-%d"), 
+                     #  Tuser_table.sex, Tuser_table.education).filter(and_(Tuser_table.education == "本科", Tuser_table.sex == "男")).all()
+
 print("条件查询")
-result = session.query(Tuser_table.username, Tuser_table.age,  func.date_format(Tuser_table.birthday, "%Y-%m-%d"), Tuser_table.sex, Tuser_table.education).filter(and_(Tuser_table.education == "本科", Tuser_table.sex == "男"),).all()
+result = session.query(Tuser_table.username, Tuser_table.age,  func.date_format(Tuser_table.birthday, "%Y-%m-%d"), 
+                       Tuser_table.sex, Tuser_table.education).filter(and_(Tuser_table.education == "本科", Tuser_table.sex == "男")).all()
 for i in result:
     print(i)
-session.commit()
-
+session.commit() 
